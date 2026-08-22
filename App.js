7661d5588adf659134adc2a52a6a73c593c767b9
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-22 16:17:04 -0700

    Add additional meshes with plane geometry
    Added two new mesh elements with plane geometry and gray materials.

--- App.js
+++ App.js
@@ -9,6 +9,14 @@ export default function App() {
         <planeGeometry  args={[3,3]}/>
         <meshBasicMaterial color="darkgray" />
       </mesh>
+      <mesh rotation={[100, 0, 0]} position={[3, 0, 0]}>
+        <planeGeometry  args={[3,3]}/>
+        <meshBasicMaterial color="gray" />
+      </mesh>
+      <mesh rotation={[100, 0, 0]} position={[0, 0, -3]}>
+        <planeGeometry  args={[3,3]}/>
+        <meshBasicMaterial color="gray" />
+      </mesh>
       {/*<mesh rotation={[0.5, 0.5, 0]} position={[0, -0, 0]} scale={[2, .5, 2]}>
         <boxGeometry />
         <meshBasicMaterial color="gray" />
