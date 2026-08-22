c5a4b3a9306c15c17d9f302c8a342fe9c381ecad
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-21 17:37:32 -0700

    Update mesh component with rotation prop

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
 
       <div style={{ height: "100vh", width: "100vw" }}>
        <Canvas>
-      <mesh>
+      <mesh rotation={[0.5, 0.5, 0]}>
         <boxGeometry />
         <meshBasicMaterial color="gray+" />
       </mesh>
