3ed3ab58292b5c0a8d995ef8bb20e63959e176e1
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-21 18:02:02 -0700

    Update App.js

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import { Canvas, useFrame } from "@react-three/fiber";
 
 export default function App() {
   return (
-
       <div style={{ height: "100vh", width: "100vw" }}>
        <Canvas>
+      <mesh rotation={[0.5, 0.5, 0]} position={[0, 1, 1]} scale={[2, .5, 2]}>
+        <planeGeometry  />
+        <meshBasicMaterial color="darkgray" />
+      </mesh>
       <mesh rotation={[0.5, 0.5, 0]} position={[0, -1, 1]} scale={[2, .5, 2]}>
         <boxGeometry />
         <meshBasicMaterial color="gray" />
