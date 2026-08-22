bf5a40219e87d92a642f8789b6c6287f45779cf8
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-22 16:46:05 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,26 +2,25 @@ import "./styles.css";
 import { Canvas, useFrame } from "@react-three/fiber";
 
 export default function App() {
+  const squares = [
+    { position: [-1, 1, 0], color: "#6B6361" },
+    { position: [1, 1, 0], color: "#94908F" },
+    { position: [-1, -1, 0], color: "#94908F" },
+    { position: [1, -1, 0], color: "#6B6361" },
+  ];
   return (
       <div style={{ height: "100vh", width: "100vw" }}>
        <Canvas>
-<group rotation={[100, 0, 0]} position={[-1.5, 0, 0]} >
-      <mesh position={[0, 0, 0]}>
-        <planeGeometry  args={[3,3]}/>
-        <meshBasicMaterial color="darkgray" />
-      </mesh>
-      <mesh rotation={[0, 0, 0]} position={[3, 0, 0]}>
-        <planeGeometry  args={[3,3]}/>
-        <meshBasicMaterial color="gray" />
-      </mesh>
-      <mesh rotation={[0, 0, 0]} position={[3, 3, 0]}>
-        <planeGeometry  args={[3,3]}/>
-        <meshBasicMaterial color="darkgray" />
-      </mesh>
-      <mesh rotation={[0, 0, 0]} position={[0, 3, 0]}>
-        <planeGeometry  args={[3,3]}/>
-        <meshBasicMaterial color="gray" />
-      </mesh>
+<group rotation={[70, 0, 0]} position={[-1.5, 0, 0]} >
+{squares.map((square, index) => (
+        <mesh key={index} position={square.position}>
+          <planeGeometry args={[2, 2]} />
+          <meshStandardMaterial
+            color={square.color}
+            
+          />
+        </mesh>
+      ))}
       </group>
       {/*<mesh rotation={[0.5, 0.5, 0]} position={[0, -0, 0]} scale={[2, .5, 2]}>
         <boxGeometry />
