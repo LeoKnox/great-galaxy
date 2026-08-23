1a700b7c80338cb9d59023dafa5f2f0b4e00f1a4
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-23 16:02:03 -0700

    Update square positions and group transformation

--- App.js
+++ App.js
@@ -3,16 +3,16 @@ import { Canvas, useFrame } from "@react-three/fiber";
 
 export default function App() {
   const squares = [
-    { position: [-1, 1, 0], color: "#8e9199" },
-    { position: [1, 1, 0], color: "#bcc0cc" },
-    { position: [-1, -1, 0], color: "#bcc0cc" },
-    { position: [1, -1, 0], color: "#8e9199" },
+    { position: [0, 2, 0], color: "#8e9199" },
+    { position: [2, 2, 0], color: "#bcc0cc" },
+    { position: [0, 0, 0], color: "#bcc0cc" },
+    { position: [2, 0, 0], color: "#8e9199" },
   ];
   return (
       <div style={{ height: "100vh", width: "100vw" }}>
        <Canvas>
        <ambientLight intensity={1} />
-<group rotation={[-70, 0, 0]} position={[0, 0, 0]} >
+<group rotation={[-70, 0, 0]} position={[-1, 0, 0]} >
 {squares.map((square, index) => (
         <mesh key={index} position={square.position}>
           <planeGeometry args={[2, 2]} />
