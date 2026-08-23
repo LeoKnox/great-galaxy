96c3aeeac4381ba9de7d237caaca3ad5ec5dafdb
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-23 15:52:54 -0700

    Update App.js

--- App.js
+++ App.js
@@ -3,20 +3,21 @@ import { Canvas, useFrame } from "@react-three/fiber";
 
 export default function App() {
   const squares = [
-    { position: [-1, 1, 0], color: "red" },
-    { position: [1, 1, 0], color: "green" },
-    { position: [-1, -1, 0], color: "blue" },
-    { position: [1, -1, 0], color: "yellow" },
+    { position: [-1, 1, 0], color: "#8e9199" },
+    { position: [1, 1, 0], color: "#bcc0cc" },
+    { position: [-1, -1, 0], color: "#bcc0cc" },
+    { position: [1, -1, 0], color: "#8e9199" },
   ];
   return (
       <div style={{ height: "100vh", width: "100vw" }}>
        <Canvas>
+       <ambientLight intensity={1} />
 <group rotation={[-70, 0, 0]} position={[0, 0, 0]} >
 {squares.map((square, index) => (
         <mesh key={index} position={square.position}>
           <planeGeometry args={[2, 2]} />
           <meshStandardMaterial
-            color={square.position}
+            color={square.color}
             
           />
         </mesh>
