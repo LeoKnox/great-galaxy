8fd06cb2795b4e628ceb6bf9cf852c88be69fae3
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-27 10:02:10 -0700

    Refactor App.js by removing Walls component
    Removed Walls component definition from App.js and adjusted Stairs positions.

--- App.js
+++ App.js
@@ -2,62 +2,10 @@ import "./styles.css";
 import { Canvas, useFrame } from "@react-three/fiber";
 import Stairs from "./components/Stairs";
 import Floor from "./components/Floor";
+import Walls from "./components/Walls";
 
 export default function App() {
-  function Walls() {
-    const wallHeight = 1;
-    const wallThickness = 0.2;
-    const roomWidth = 12;
-    const roomDepth = 8;
   
-    return (
-      <group>
-        {/* Back wall */}
-        <mesh
-          position={[0, wallHeight / 2, -roomDepth / 2]}
-          castShadow
-        >
-          <boxGeometry
-            args={[roomWidth, wallHeight, wallThickness]}
-          />
-          <meshStandardMaterial color="#3b3d42" />
-        </mesh>
-  
-        {/* Front wall */}
-        <mesh
-          position={[0, wallHeight / 2, roomDepth / 2]}
-          castShadow
-        >
-          <boxGeometry
-            args={[roomWidth, wallHeight, wallThickness]}
-          />
-          <meshStandardMaterial color="#3b3d42" />
-        </mesh>
-  
-        {/* Left wall */}
-        <mesh
-          position={[-roomWidth / 2, wallHeight / 2, 0]}
-          castShadow
-        >
-          <boxGeometry
-            args={[wallThickness, wallHeight, roomDepth]}
-          />
-          <meshStandardMaterial color="#3b3d42" />
-        </mesh>
-  
-        {/* Right wall */}
-        <mesh
-          position={[roomWidth / 2, wallHeight / 2, 0]}
-          castShadow
-        >
-          <boxGeometry
-            args={[wallThickness, wallHeight, roomDepth]}
-          />
-          <meshStandardMaterial color="#3b3d42" />
-        </mesh>
-      </group>
-    );
-  }
   function Room() {
     return (
       <>
@@ -66,11 +14,11 @@ export default function App() {
   
         {/* Reusable stair instances */}
         <Stairs
-        position={[-3, 0, 0]}
+        position={[-3.5, 0, .5]}
         rotation={[0, 0, 0]}
       />
         <Stairs
-        position={[3, 0, 1]}
+        position={[3.5, 0, -1.5]}
         rotation={[0, Math.PI / 2, 0]}
         color="#77503b"
       />
