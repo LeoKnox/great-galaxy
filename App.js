09bd288b56fdf6dfc4f5bd81af0038d1c222c864
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-28 18:09:27 -0700

    Refactor App.js by removing Stairs and Floor components
    Removed unused components and their instances from App.js.

--- App.js
+++ App.js
@@ -1,30 +1,11 @@
 import "./styles.css";
 import { Canvas, useFrame } from "@react-three/fiber";
-import Stairs from "./components/Stairs";
-import Floor from "./components/Floor";
-import Walls from "./components/Walls";
+
+import Room from "./components/Room";
 
 export default function App() {
   
-  function Room() {
-    return (
-      <>
-        <Floor width={12} depth={8} />
-        <Walls />
   
-        {/* Reusable stair instances */}
-        <Stairs
-        position={[-3.5, 0, .5]}
-        rotation={[0, 0, 0]}
-      />
-        <Stairs
-        position={[3.5, 0, -1.5]}
-        rotation={[0, Math.PI / 2, 0]}
-        color="#77503b"
-      />
-      </>
-    );
-  }
   
   return (
       <div style={{ height: "100vh", width: "100vw" }}>
