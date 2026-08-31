dc0d3c80e0367fe916cc048e2b40ef776b226f0e
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-30 21:23:23 -0700

    Refactor Character component to use mesh

--- components/Character.js
+++ components/Character.js
@@ -2,9 +2,9 @@ export default function Character(position = [0, 0, 0],
     rotation = [0, 0, 0],
 ) {
     return (
-    <group position={position} rotation={rotation}>
+        <mesh position={[0, 0, 0]} rotation={[0, 0, 0]}>
          <coneGeometry args={[1, 2, 32]} /> 
       <meshStandardMaterial color="blue" />
-    </group>
+    </mesh>
     )
 }
