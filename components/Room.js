96969d9c7c2ad5d1e1147f394b9562859734fe21
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-31 16:36:24 -0700

    Update Room.js

--- components/Room.js
+++ components/Room.js
@@ -2,8 +2,14 @@ import Stairs from "./Stairs";
 import Floor from "./Floor";
 import Walls from "./Walls";
 import Character from "./Character";
+import {useState} from "react";
 
 export default function Room() {
+    const [characterPosition, setCharacterPosition] = useState({position:[
+        -0.5,
+        0,
+        -1.5,
+      ]});
     const stairsLoc = [{position:[-3.5, 0, .5], rotation:[0, 0, 0]},
     {position:[3.5, 0, -1.5], rotation:[0, Math.PI / 2, 0],color:"lightGray"}]
     return (
@@ -18,7 +24,7 @@ export default function Room() {
       color= {stairsLoc[v].color}
       />
     ))}
-    <Character position={[-.5, 0, -1.5]} rotation={[0, 0, 0]}/>
+    <Character position={[...characterPosition.position]} rotation={[0, 0, 0]}/>
       </>
     );
   }
