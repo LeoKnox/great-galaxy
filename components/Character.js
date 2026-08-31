8ba386b5d168e6d5061a967b04cd05a9fff2b88f
LeoKnox <LeoKnox@users.noreply.github.com>
2026-08-30 21:25:13 -0700

    Refactor Character component to use props

--- components/Character.js
+++ components/Character.js
@@ -1,8 +1,8 @@
-export default function Character(position = [0, 0, 0],
-    rotation = [0, 0, 0],
+export default function Character({position = [0, 0, 0],
+    rotation = [0, 0, 0],}
 ) {
     return (
-        <mesh position={[0, 0, 0]} rotation={[0, 0, 0]}>
+        <mesh position={position} rotation={rotation}>
          <coneGeometry args={[1, 2, 32]} /> 
       <meshStandardMaterial color="blue" />
     </mesh>
