84f750c55e55cacc046cf84da77360643b6253bf
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-04 15:48:17 -0700

    Refactor App.js for improved readability

--- App.js
+++ App.js
@@ -4,37 +4,34 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import Room from "./components/Room";
 
 export default function App() {
-  
-  
-  
   return (
-      <div style={{ height: "100vh", width: "100vw" }}>
-<Canvas
-      orthographic
-      shadows
-      camera={{
-        position: [0, 12, 0],
-        rotation: [-Math.PI / 2, 0, 0],
-        zoom: 65,
-        near: 0.1,
-        far: 100,
-      }}
-    >
-      <color attach="background" args={["#202228"]} />
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Canvas
+        orthographic
+        shadows
+        camera={{
+          position: [0, 12, 0],
+          rotation: [-Math.PI / 2, 0, 0],
+          zoom: 65,
+          near: 0.1,
+          far: 100,
+        }}
+      >
+        <color attach="background" args={["#202228"]} />
 
-      <ambientLight intensity={1.5} />
+        <ambientLight intensity={1.5} />
 
-      <directionalLight
-        position={[5, 10, 5]}
-        intensity={2}
-        castShadow
-        shadow-mapSize-width={2048}
-        shadow-mapSize-height={2048}
-      />
+        <directionalLight
+          position={[5, 10, 5]}
+          intensity={2}
+          castShadow
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
+        />
 
-      <Room />
-    </Canvas>
+        <Room />
+      </Canvas>
+      <h3>Character Name</h3>
     </div>
-
   );
 }
