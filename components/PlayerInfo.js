b9ac5c66a5dea2ceae7cea1a1f3162213529264d
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-05 17:43:50 -0700

    Update PlayerInfo component to accept character prop

--- components/PlayerInfo.js
+++ components/PlayerInfo.js
@@ -1,7 +1,7 @@
-export default function PlayerInfo() {
+export default function PlayerInfo({ character }) {
   return (
     <div className="playerInfo">
-      <h3>Player Name</h3>
+      <h3>{character.name}</h3>
     </div>
   );
 }
