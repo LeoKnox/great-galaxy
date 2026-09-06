3757570a6de807e43fd9a2607618f55de97e62b7
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-05 17:47:44 -0700

    Add progress bar for character health in PlayerInfo

--- components/PlayerInfo.js
+++ components/PlayerInfo.js
@@ -4,6 +4,8 @@ export default function PlayerInfo({ character }) {
       <h3>
         {character.name} | {character.level} {character.class}
       </h3>
+      <progress id="hp" max={character.hp} value={character.hp} />
     </div>
   );
 }
+
