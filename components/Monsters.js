886cfc6e4767ca7fa2d845ca8b50d2fdd25d065f
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-06 15:59:03 -0700

    Update Monsters component to support multiple positions

--- components/Monsters.js
+++ components/Monsters.js
@@ -1,11 +1,14 @@
 export default function Monsters({
-  position = [[0.5, 0, 0.5]],
+  position = [
+    [0.5, 0, 0.5],
+    [1.5, 0, 2.5],
+  ],
   rotation = [0, 0, 0],
 }) {
-  return (
-    <mesh position={position[0]}>
+  return position.map((cur) => (
+    <mesh position={cur}>
       <sphereGeometry args={[0.5, 16, 16]} />
       <meshStandardMaterial color="indigo" />
     </mesh>
-  );
+  ));
 }
