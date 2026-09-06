54e838ec1b2de0dac32fd832efb8738e256ea7ab
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-06 15:48:32 -0700

    Update Monsters component to use nested position array

--- components/Monsters.js
+++ components/Monsters.js
@@ -1,9 +1,9 @@
 export default function Monsters({
-  position = [0, 0, 0],
+  position = [[0.5, 0, 0.5]],
   rotation = [0, 0, 0],
 }) {
   return (
-    <mesh position={[0, 0, 0]}>
+    <mesh position={position[0]}>
       <sphereGeometry args={[0.5, 16, 16]} />
       <meshStandardMaterial color="indigo" roughness={0.2} />
     </mesh>
