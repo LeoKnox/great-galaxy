f4d5a70690072cab4a4b28ef21d29ebee881481b
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-05 17:55:59 -0700

    Add tab space to PlayerInfo component

--- components/PlayerInfo.js
+++ components/PlayerInfo.js
@@ -2,10 +2,9 @@ export default function PlayerInfo({ character }) {
   return (
     <div className="playerInfo">
       <h3>
-        {character.name} | {character.level} {character.class}
+        {character.name} | {character.level} {character.class} {"\t"}
+        <progress id="hp" max={character.hp} value={character.hp} />
       </h3>
-      <progress id="hp" max={character.hp} value={character.hp} />
     </div>
   );
 }
-
