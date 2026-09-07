b43a7f5e5f0a2576422687808599c5b5e702af36
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-07 14:24:21 -0700

    Refactor Character component to include PlayerInfo
    Added PlayerInfo component and changed cone color to green.

--- components/Character.js
+++ components/Character.js
@@ -1,10 +1,16 @@
-export default function Character({position = [0, 0, 0],
-    rotation = [0, 0, 0],}
-) {
-    return (
-        <mesh position={position} rotation={rotation}>
-         <coneGeometry args={[1, 2, 32]} /> 
-      <meshStandardMaterial color="blue" />
-    </mesh>
-    )
+import PlayerInfo from "./PlayerInfo";
+
+export default function Character({
+  position = [0, 0, 0],
+  rotation = [0, 0, 0],
+}) {
+  return (
+    <>
+      <PlayerInfo />
+      <mesh position={position} rotation={rotation}>
+        <coneGeometry args={[1, 2, 16]} />
+        <meshStandardMaterial color="green" />
+      </mesh>
+    </>
+  );
 }
