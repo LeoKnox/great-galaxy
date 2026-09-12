2dda74eecf547e323ba645fe1c4e0bb979cf87a8
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-12 16:45:55 -0700

    Fix collision check for stairs location

--- components/Room.js
+++ components/Room.js
@@ -23,7 +23,7 @@ export default function Room() {
   function collision(x = -2.5, y = 0, z = -2.5, offsetx, offsety, offsetz) {
     let temp = [x + offsetx, offsety, z + offsetz];
     console.log(stairsLoc[0].position);
-    if (stairsLoc.every((temp, index) => temp === temp[index].position)) {
+    if (stairsLoc.every((temp, index) => temp === stairsLoc[index].position)) {
       console.log("Stairs");
       return [x + offsetx, 2, z + offsetz];
     }
