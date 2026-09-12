ae60e5e35bbf132cf104ec7d0be49fed810ede0e
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-12 16:41:16 -0700

    Remove console log for stairsLoc position
    Remove unnecessary console log statement for stairs location.

--- components/Room.js
+++ components/Room.js
@@ -22,7 +22,7 @@ export default function Room() {
 
   function collision(x = -2.5, y = 0, z = -2.5, offsetx, offsety, offsetz) {
     let temp = [x + offsetx, offsety, z + offsetz];
-
+    console.log(stairsLoc[0].position);
     if (stairsLoc.every((temp, index) => temp === temp[index].position)) {
       console.log("Stairs");
       return [x + offsetx, 2, z + offsetz];
