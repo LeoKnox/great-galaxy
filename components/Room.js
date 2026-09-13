aeefbb6f154908126d7fa80c5d356d680717772b
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-13 16:17:03 -0700

    Fix collision detection logic in Room.js
    Refactor collision function to check for stairs location correctly.

--- components/Room.js
+++ components/Room.js
@@ -23,11 +23,16 @@ export default function Room() {
   function collision(x = -2.5, y = 0, z = -2.5, offsetx, offsety, offsetz) {
     let temp = [x + offsetx, offsety, z + offsetz];
     console.log(stairsLoc[0].position);
-    if (stairsLoc.every((value, index) => temp !== stairsLoc[index].position)) {
+    /*if (stairsLoc.every((value, index) => temp !== stairsLoc[index].position)) {
       console.log("Stairs");
-      console.log(value + ":" + index + ":" + stairsLoc[index].position);
+      console.log({ value } + ":" + index + ":" + stairsLoc[index].position);
       return [x + offsetx, 2, z + offsetz];
-    }
+    }*/
+    let c = stairsLoc.every(
+      (value, index) => temp === stairsLoc[index].position
+    );
+
+    console.log(c);
     return temp;
   }
 
