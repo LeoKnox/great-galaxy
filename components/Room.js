a7b922d3dfe39c7c7c7decf36f01a32f10aa962b
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-13 16:05:59 -0700

    Change collision condition to check for inequality

--- components/Room.js
+++ components/Room.js
@@ -23,8 +23,9 @@ export default function Room() {
   function collision(x = -2.5, y = 0, z = -2.5, offsetx, offsety, offsetz) {
     let temp = [x + offsetx, offsety, z + offsetz];
     console.log(stairsLoc[0].position);
-    if (stairsLoc.every((value, index) => temp === stairsLoc[index].position)) {
+    if (stairsLoc.every((value, index) => temp !== stairsLoc[index].position)) {
       console.log("Stairs");
+      console.log(value + ":" + index + ":" + stairsLoc[index].position);
       return [x + offsetx, 2, z + offsetz];
     }
     return temp;
