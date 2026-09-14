ac48f72d81b6d1371d88c9428a78a294cf856e72
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-14 14:48:21 -0700

    Simplify position comparison in collision function
    Refactor collision function to simplify position comparison.

--- components/Room.js
+++ components/Room.js
@@ -23,9 +23,7 @@ export default function Room() {
   function collision(x = -2.5, y = 0, z = -2.5, offsetx, offsety, offsetz) {
     let temp = [x + offsetx, offsety, z + offsetz];
     let c = stairsLoc.some((value, index) =>
-      value.position.every(
-        (coord, loc) => coord.position == characterPosition.position
-      )
+      value.position.every((coord, loc) => coord == characterPosition.position)
     );
     return temp;
   }
