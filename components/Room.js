520a26f4d7ab3d844a9d63ff4f63a45f66583ce0
LeoKnox <LeoKnox@users.noreply.github.com>
2026-09-14 17:01:57 -0700

    Update Room.js

--- components/Room.js
+++ components/Room.js
@@ -23,9 +23,7 @@ export default function Room() {
   function collision(x = -2.5, y = 0, z = -2.5, offsetx, offsety, offsetz) {
     let temp = [x + offsetx, offsety, z + offsetz];
     let c = stairsLoc.some((value, index) =>
-      value.position.every(
-        (coord, loc) => coord == characterPosition.position[loc]
-      )
+      value.position.every((coord, loc) => coord == temp)
     );
     console.log(c);
     return temp;
